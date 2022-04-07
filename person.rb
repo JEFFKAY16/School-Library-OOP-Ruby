db72e092b6278ecabb9349b2f5cef98c4a684b29
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-04-07 21:59:45 +0200

    Add basic UI

--- person.rb
+++ person.rb
@@ -1,34 +1,38 @@
-require './corrector'
-require './rental'
+# rubocop: disable Style/OptionalBooleanParameter
+
+require_relative 'corrector'
+require_relative 'rental'
 
 class Person
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1...1000)
-    @name = name
+  def initialize(age, name = 'Unknown', parent_permission = true)
+    @id = Random.rand(1..1000)
     @age = age
+    @name = name
     @parent_permission = parent_permission
     @corrector = Corrector.new
     @rentals = []
   end
 
+  def of_age?
+    @age >= 18
+  end
+
   def can_use_services?
-    of_age? || @parent_permission ? true : false
+    of_age? || @parent_permission
   end
 
   def validate_name
     @name = @corrector.correct_name(@name)
   end
 
-  def add_rentals(book, date)
-    Rental.new(date, self, book)
+  def add_rentals(date, book)
+    Rental.new(date, book, self)
   end
 
-  private
-
-  def of_age?
-    @age >= 18
-  end
+  private :of_age?
 end
+
+# rubocop: enable Style/OptionalBooleanParameter
