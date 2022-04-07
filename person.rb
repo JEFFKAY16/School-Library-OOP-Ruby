1d891e10360c9a7af4122e49902913b013d99f69
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-04-07 17:50:09 +0200

    Set up associations

--- person.rb
+++ person.rb
@@ -1,24 +1,29 @@
-require './nameable'
-require './decorator'
+require './corrector'
+require './rental'
 
-class Person < Nameable
-  def initialize(age, name = 'Unknown', parrent_permission: true)
-    super()
-    @id = Random.rand(1..1000)
-    @name = name
-    @age = age
-    @parrent_permission = parrent_permission
-  end
-
-  attr_accessor :name, :age
+class Person
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def can_use_services?
-    is_of_age? || @parent_permission
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1...1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @corrector = Corrector.new
+    @rentals = []
   end
 
-  def correct_name
-    @name
+  def can_use_services?
+    of_age? || @parent_permission ? true : false
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
+  end
+
+  def add_rentals(book, date)
+    Rental.new(date, self, book)
   end
 
   private
